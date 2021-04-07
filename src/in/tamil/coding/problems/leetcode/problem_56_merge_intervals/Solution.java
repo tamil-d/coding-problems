dd42dbc9912fa81d88d9d0aaf8aa89fa818794cd
Tamilarasan Dharmalingam <tamilarasan88@gmail.com>
2021-04-06 21:59:32 -0400

    Initial Commit. Solution for Leetcode Problem 56

--- src/in/tamil/coding/problems/leetcode/problem_56_merge_intervals/Solution.java
+++ src/in/tamil/coding/problems/leetcode/problem_56_merge_intervals/Solution.java
@@ -1,17 +1,17 @@
-package in.tamil.coding.problems.leetcode.problem_56;
+package in.tamil.coding.problems.leetcode.problem_56_merge_intervals;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         LinkedList<int[]> mergedIntervals = new LinkedList<>();
-        for(int[] currInterval : intervals) {
+        for (int[] currInterval : intervals) {
             if (mergedIntervals.isEmpty() || mergedIntervals.getLast()[1] < currInterval[0]) {
                 mergedIntervals.add(currInterval);
             } else {
-                mergedIntervals.getLast()[1] = Math.max(mergedIntervals.getLast()[1],currInterval[1]);
+                mergedIntervals.getLast()[1] = Math.max(mergedIntervals.getLast()[1], currInterval[1]);
             }
         }
         return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
