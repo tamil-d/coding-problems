e8f91fe620177e4c7b34280f6461843a4211a3a7
Tamilarasan Dharmalingam <tamilarasan88@gmail.com>
2021-05-16 01:30:31 -0400

    employee free time problem

---
+++ src/in/tamil/coding/problems/leetcode/problem_759_emp_free_time/Solution.java
@@ -0,0 +1,49 @@
+package in.tamil.coding.problems.leetcode.problem_759_emp_free_time;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        List<Interval> result = new ArrayList<>();
+        List<Interval> sortedSchedule = new ArrayList<>();
+        for (List<Interval> list : schedule) {
+            sortedSchedule.addAll(list);
+        }
+        if (sortedSchedule.isEmpty()) {
+            return result;
+        }
+        sortedSchedule.sort((int1, int2) -> int1.start - int2.start);
+        LinkedList<Interval> mergedIntervals = new LinkedList<>();
+        for (Interval currSch : sortedSchedule) {
+            if (mergedIntervals.isEmpty()) {
+                mergedIntervals.add(currSch);
+                continue;
+            }
+            Interval lastIntInList = mergedIntervals.getLast();
+            if (currSch.start <= mergedIntervals.getLast().end) {
+
+                lastIntInList.start = Math.min(currSch.start, lastIntInList.start);
+                lastIntInList.end = Math.max(currSch.end, lastIntInList.end);
+            } else {
+                result.add(new Interval(lastIntInList.end, currSch.start));
+                mergedIntervals.add(currSch);
+            }
+        }
+        return result;
+    }
+
+    private class Interval {
+        public int start;
+        public int end;
+
+        public Interval() {
+        }
+
+        public Interval(int _start, int _end) {
+            start = _start;
+            end = _end;
+        }
+    }
+}
